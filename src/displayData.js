4a3b486b2871ccd2ef8523d7ad76d75ea56c2789
Gulcan Damdelen <61693319+Gulcan00@users.noreply.github.com>
2023-12-30 20:36:52 +0200

    Add displayForecast function and update displayWeather to include forecast

--- src/displayData.js
+++ src/displayData.js
@@ -1,5 +1,41 @@
 import { getWeather } from "./fetchData";
 
+function displayForecast(forecast) {
+  const div = document.createElement("div");
+  div.classList.add("forecast");
+  forecast.forEach((day) => {
+    const dayDiv = document.createElement("div");
+    const date = document.createElement("p");
+    date.innerText = day.date;
+    dayDiv.appendChild(date);
+    div.appendChild(dayDiv);
+  });
+  return div;
+}
+
+function displayCurrentWeather(current) {
+  const div = document.createElement("div");
+  div.classList.add("current");
+
+  const icon = document.createElement("img");
+  icon.src = current.condition.icon;
+  div.appendChild(icon);
+
+  const temp = document.createElement("p");
+  temp.innerText = `${current.temp_c}°C`;
+  div.appendChild(temp);
+
+  const humidity = document.createElement("p");
+  humidity.innerText = `${current.humidity}%`;
+  div.appendChild(humidity);
+
+  const wind = document.createElement("p");
+  wind.innerText = `${current.wind_kph} km/h`;
+  div.appendChild(wind);
+
+  return div;
+}
+
 function displayLocation({ country, localtime, name }) {
   const container = document.getElementById("container");
   const div = document.createElement("div");
@@ -39,34 +75,16 @@ function displayLocation({ country, localtime, name }) {
   return div;
 }
 
-function displayCurrentWeather(current) {
-  const div = document.createElement("div");
-  div.classList.add("current");
-
-  const icon = document.createElement("img");
-  icon.src = current.condition.icon;
-  div.appendChild(icon);
-
-  const temp = document.createElement("p");
-  temp.innerText = `${current.temp_c}°C`;
-  div.appendChild(temp);
-
-  const humidity = document.createElement("p");
-  humidity.innerText = `${current.humidity}%`;
-  div.appendChild(humidity);
-
-  const wind = document.createElement("p");
-  wind.innerText = `${current.wind_kph} km/h`;
-  div.appendChild(wind);
-
-  return div;
-}
-
 export default async function displayWeather() {
-  const { location, current, forecast } = await getWeather();
+  const {
+    location,
+    current,
+    forecast: { forecastday },
+  } = await getWeather();
   console.log(current);
-  console.log(forecast);
+  console.log(forecastday);
   const container = document.getElementById("container");
   container.appendChild(displayLocation(location));
   container.appendChild(displayCurrentWeather(current));
+  container.appendChild(displayForecast(forecastday));
 }
