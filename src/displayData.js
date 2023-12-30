7ffb1742580d19212e8609369b71a81e8003081b
Gulcan Damdelen <61693319+Gulcan00@users.noreply.github.com>
2023-12-30 19:25:12 +0200

    Add displayCurrentWeather function to display current weather information

--- src/displayData.js
+++ src/displayData.js
@@ -20,7 +20,28 @@ function displayLocation({ country, localtime, name }) {
   return div;
 }
 
-function displayCurrentWeather(current) {}
+function displayCurrentWeather(current) {
+  const div = document.createElement("div");
+  div.classList.add("current");
+
+  const icon = document.createElement("img");
+  icon.src = current.condition.icon;
+  div.appendChild(icon);
+
+  const temp = document.createElement("p");
+  temp.innerText = `${current.temp_c}°C`;
+  div.appendChild(temp);
+
+  const humidity = document.createElement("p");
+  humidity.innerText = `${current.humidity}%`;
+  div.appendChild(humidity);
+
+  const wind = document.createElement("p");
+  wind.innerText = `${current.wind_kph} km/h`;
+  div.appendChild(wind);
+
+  return div;
+}
 
 export default async function displayWeather() {
   const { location, current, forecast } = await getWeather();
@@ -28,4 +49,5 @@ export default async function displayWeather() {
   console.log(forecast);
   const container = document.getElementById("container");
   container.appendChild(displayLocation(location));
+  container.appendChild(displayCurrentWeather(current));
 }
